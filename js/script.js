83f0bd68417c32d1785394be52f532d6f32fc0e1
osamahabibm <40914381+osamahabibm@users.noreply.github.com>
2021-10-01 23:24:39 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // Navbar Scroll
+    $(window).on("scroll", function () {
+        var scroll = $(window).scrollTop();
+        if(scroll >= 50) {
+            $(".sticky").addClass("stickyadd");
+        } else {
+            $(".sticky").removeClass("stickyadd");
+        }
+    });
+
+   
+});
+    
